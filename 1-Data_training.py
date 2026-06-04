9166226d7f0f65e12fe5e88828f7fb1b7fe05e25
Maddalena Berruti <maddy.berruti@gmail.com>
2026-06-04 16:34:21 +0200

    Script for training the data collected previously

---
+++ 1-Data_training.py
@@ -0,0 +1,68 @@
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import numpy as np
+import os
+from tensorflow.keras.models import Sequential       
+from tensorflow.keras.layers import LSTM, Dense      
+                                                     
+from tensorflow.keras.callbacks import TensorBoard    
+
+DATA_PATH = os.path.join('MP_Data')  
+actions = os.listdir(DATA_PATH)
+label_map = {label:num for num, label in enumerate(actions)}
+print(f"Actions list: {actions}")
+
+i = 0
+no_sequences = np.zeros((len(actions)))
+sequence_length = np.zeros((len(actions)))
+for action in actions:
+    no_sequences[i] = int(len(os.listdir(f"./MP_Data/{action}")))
+    sequence_length[i] = int(len(os.listdir(f"./MP_Data/{action}/0")))
+    i+=1
+print(f"Actions have {no_sequences} sequences with {sequence_length} frames each")
+
+i = 0
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(int(no_sequences[i])):
+        window = []
+        for frame_num in range(int(sequence_length[i])):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)                               # List of landmarks position for one single video
+        sequences.append(window)                             # List of landmark positions for each sign in all videos
+        labels.append(label_map[action])
+    i+=1
+
+# --------------------------------- Create train and test set ----------------------------------------
+y = np.eye(len(np.unique(labels)))[np.array(labels).astype(int)]
+X = np.array(sequences)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.05)
+
+# --------------------------------- Create the neural network ----------------------------------------
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+n_keypoints = X.shape[2]
+
+model = Sequential()        
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(sequence_length, n_keypoints)))
+# Syntax: model.add(LSTM(number of LSTM units, 
+#                   return_sequences which is necessaty with tensorflow because it allows to stack sequences and the next layer will need 
+#                   activation type
+#                   input_shape(number of frames, number of keypoints)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu')) # In this case don't return the sequence because the next layer is dense
+model.add(Dense(64, activation='relu'))                        # 64 units of densely connected neural network neurons
+model.add(Dense(128, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))       
+
+model.compile(optimizer='Adam', loss = 'categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, y_train, epochs= 100, callbacks=[tb_callback])
+model.save('action.h5')
+                                                            
+                                                            
+                                                            
+                                                               
+
+
+
+
